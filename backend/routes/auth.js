b714885ba78b431d0c6237dec3a39c1799098647
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-07-26 13:34:23 +0530

    added password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,19 +1,24 @@
 const express=require("express")
 const Users=require("../models/Users")
-
+const bcrypt=require("bcrypt")
 const router=express.Router()
+const saltRounds=10
 
 
 router.post("/signup",async(req,res,next)=>{
     try {
         const body=req.body
         const username=body.username
+        const email=body.email
         const user=await Users.findOne({where:{username:username}})
         if(user)return res.status(202).json("User already exist")
+        const useremail=await Users.findOne({where:{email:email}})
+        if(useremail)return res.status(202).json("User with email already exist")
+       const hasdhedPass=await bcrypt.hash(body.password,saltRounds)
         await Users.create({
          username:body.username,
          email:body.email,
-         password:body.password
+         password:hasdhedPass
         })
         res.json("User created succesfully")
     } catch (error) {
@@ -28,7 +33,9 @@ router.post("/login",async(req,res,next)=>{
     if(!user){
         return res.status(401).json("Email doesnt exist")
     }
-    if(user.password!==password) return res.status(404).json("Password is incorrect")
+
+    const matched=await bcrypt.compare(password,user.password)
+    if(!matched) return res.status(404).json("Password is incorrect")
     res.status(202).json("Login Successful")
 })
 
